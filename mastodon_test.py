7fc5549bec94431e2cb758570ac8a59351b11162
Latti31 <111042692+Latti31@users.noreply.github.com>
2023-05-02 11:41:05 +1000

    Created a test app (saves ref to file)

--- mastodon_test.py
+++ mastodon_test.py
@@ -1,2 +1,11 @@
 from mastodon import Mastodon
-print("working")
+
+Mastodon.create_app(
+    'latti_test_app',
+    api_base_url= 'Mastodon.social',
+    to_file='test_mastodon_save'
+)
+
+mastodon = Mastodon(client_id='test_mastodon_save')
+
+print(mastodon)
