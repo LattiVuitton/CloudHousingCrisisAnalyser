0948910f34dea81adb6f37a29a2dc8545b2cd415
Latti31 <111042692+Latti31@users.noreply.github.com>
2023-05-14 19:54:50 +1000

    multi-server bug fix

--- Mastodon/mastodon_test_3.py
+++ Mastodon/mastodon_test_3.py
@@ -16,9 +16,6 @@ os.makedirs(logs_folder, exist_ok=True)
 # Configure logging to write to the error log file
 logging.basicConfig(filename=os.path.join(logs_folder, 'errors.log'), level=logging.ERROR)
 
-logging.error("Code started Test Again")
-logging.debug("Code started (debug)")
-
 # logging.basicConfig(filename='C:/Users/jonat/OneDrive/Documents/University/2023_Semester_1/Cloud_Computing/Project_2/CloudHousingCrisisAnalyser/Mastodon/my_log', level=logging.ERROR) # local
 
 # For running on server -> $ nohup python3 mastodon_scraper.py &
@@ -52,14 +49,11 @@ db_url = 'http://admin:mysecretpassword@172.26.135.198:5984/'
 dbname = 'mastodon_test_db'
 
 def pushToCouch(data):
-    logging.error("Attempting to push to couch")
 
     # Connect to the CouchDB server using the .pem file
     server = couchdb.Server(db_url)
 
     while True:
-        logging.error("Attempting to push to couch (in loop)")
-
         try:
             # Select the database to use
             db = server[dbname]
@@ -70,22 +64,17 @@ def pushToCouch(data):
             break
 
         except couchdb.http.ServerError as e1:
-            print("Failed to connect to CouchDB. Retrying...")
             logging.error("err1: %s", str(e1))
 
         except Exception as e:
             logging.error("err2: %s", str(e))
-            print("An error occurred while saving document to CouchDB:", e)
             break
 
 # Retrieves posts since last get request
 def getRecent():
-    logging.error("Attempting to get recent posts")
-
     is_end = False
     all_posts = []
 
-    logging.error("All urls:" + str(len(URLS)))
     for ur in URLS:
         logging.error("> %d", str(ur))
 
@@ -94,15 +83,11 @@ def getRecent():
         server_url = url_pair[0]
         region = url_pair[1]
 
-        logging.error("URL: %d.", str(server_url))
-        logging.error("Found %d all_posts.", len(all_posts))
-
         params = {'limit': 40}
         r = requests.get(server_url, params=params)
         posts = json.loads(r.text)
 
         if len(posts) == 0:
-            logging.error("Breaking out with 0 posts")
             break
 
         for post in posts:
@@ -115,31 +100,21 @@ def getRecent():
             all_posts.append(post)
         
         if is_end:
-            logging.error("Breaking out since end")
             break
 
         max_id = posts[-1]['id']
         params['max_id'] = max_id
 
-    logging.error("Successfully returning")
     return pd.DataFrame(all_posts)
 
-logging.error("Starting main loop")
-
 while True:
-    logging.error("Main loop")
 
     try:
         posts = getRecent()
-        logging.error("Made it through")
 
     except Exception as e3:
         posts = []
         logging.error("err1", str(e3))
-
-        print("Potential connection issue (No posts retrieved from mastodon)")
-
-    logging.error("Found %d posts.", len(posts))
 
     if len(posts) > 0:
         start_time = pd.Timestamp(posts.loc[len(posts)-1]['created_at'], tz='utc')
@@ -165,16 +140,6 @@ while True:
                 }
 
                 pushToCouch(post_json)
-
-                logging.error("Pushed post %d.", i)
-                # print("Pushed a post")
-                # logging.info("Nice, I just pushed a post!", post_json['created_at'])
-                # print("Nice, I just pushed a post!", post_json['created_at'])
-                # print("print test")
-                # logging.info("Not printing")
-
-            else:
-                logging.error("Duplicate post %d.", i)
 
         set_1 = copy.deepcopy(set_2)
         set_2.clear()
