571a926f164931649d8e95f26e08792072f18dbe
Latti31 <111042692+Latti31@users.noreply.github.com>
2023-05-17 23:03:33 +1000

    Get hourly closing prices

--- Mastodon/Docker/historical_economy_figures.py
+++ Mastodon/Docker/historical_economy_figures.py
@@ -6,16 +6,31 @@ TICKERS = [
     # Australian Economy
     yf.Ticker("^AORD"),     # All Ordinaries
     yf.Ticker("^AXJO"),     # ASX 200
-    yf.Ticker("^AUDUSD=X"), # Aussie Dollar
+    yf.Ticker("AUDUSD=X"),  # Aussie Dollar
 
     # Global Economy
     yf.Ticker("^GSPC"), # S&P 500
     yf.Ticker("^DJI"),  # Dow Jones Industrial Average
-    yf.Ticker("FTSE"),  # FTSE 100
+    yf.Ticker("^FTSE"), # FTSE 100
     yf.Ticker("^N225"), # Nikkei 225
 
     # Misc
     yf.Ticker("BTC-USD"), # Bitcoin
-    yf.Ticker("XAU=X"),   # Gold Price
-    yf.Ticker("DXY"),     # American Dollar
+    yf.Ticker("GC=F")    # Gold Price
 ]
+
+# Set the start and end dates for the data
+start_date = "2022-05-17"
+end_date = "2023-05-17"
+
+# Define the interval for data retrieval
+interval = "1h"  # Daily data
+
+# Download the data for each ticker
+for ticker in TICKERS[:1]:
+    data = ticker.history(start=start_date, end=end_date, interval=interval)
+    print(f"Stock data for ticker '{ticker.ticker}': '{len(data)}'")
+
+print("\n")
+for line in data['Close']:
+    print(line)
