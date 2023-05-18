6b049055f3848b8281908caa5900eed6bb8b8888
adesu7 <adityadesu100@gmail.com>
2023-05-18 14:53:07 +1000

    updated sudo to main

--- sudo-data-processing/sudo-processing.py
+++ sudo-data-processing/sudo-processing.py
@@ -13,6 +13,9 @@ url = "http://" + username + ":" + password + "@" + db
 headers = {'Content-type':'application/json'}
 
 
+import numpy as np
+import pandas as pd
+
 # Iterate over each file in the directory and subdirectories
 for root, dirs, files in os.walk(sudo_rental_data_path):
     for filename in files:       
@@ -22,17 +25,25 @@ for root, dirs, files in os.walk(sudo_rental_data_path):
             # Load the JSON data
             with open(file_path, 'r') as f:
                 data = json.load(f)
+            
+            valid_data = []
             for doc in data['features']:
-                # Ensure each document has a unique _id
-                doc['_id'] = str(doc['properties']['unique_id'])
+                q2_2019 = doc['properties'].get('rai_cityadjusted_total_2019_q2')
+                q2_2021 = doc['properties'].get('rai_cityadjusted_total_2021_q2')
 
-            bulk_data = {'docs': data}
+                # Ensure each document has a unique _id and not null for 'rai_cityadjusted_total_2021_q2' and 'rai_cityadjusted_total_2019_q2'
+                if q2_2019 is not None and q2_2021 is not None:
+                    doc['_id'] = str(doc['properties']['geography_name'])
+                    valid_data.append(doc)
 
-            # Send the data to CouchDB
-            response = requests.post(url, headers=headers, json=bulk_data)
+                #bulk_data = {'docs': valid_data}
 
-            # Check if the POST request was successful
-            if response.status_code == 201:
-                print(f'Successfully uploaded {filename} to CouchDB')
-            else:
-                print(f'Failed to upload {filename}. Status code: {response.status_code}. Message: {response.text}')
+                    # Send the data to CouchDB
+                    response = requests.post(url, headers=headers, json=doc)
+
+                    # Check if the POST request was successful
+                    if response.status_code == 201:
+                        print(f'Successfully uploaded {filename} to CouchDB')
+                    else:
+                        print(f'Failed to upload {filename}. Status code: {response.status_code}. Message: {response.text}')
+ 
