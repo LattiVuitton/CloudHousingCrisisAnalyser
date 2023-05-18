8952c0863fd3482a9c72514871463cca3cc2ef5e
Latti31 <111042692+Latti31@users.noreply.github.com>
2023-05-18 14:08:55 +1000

    Cleaned code and pushed to test DB

--- Mastodon/Docker/historical_economy_figures.py
+++ Mastodon/Docker/historical_economy_figures.py
@@ -1,5 +1,7 @@
 import yfinance as yf
 import json
+import couchdb
+import traceback
 
 # Create a ticker object for each asset
 TICKERS = [
@@ -21,36 +23,81 @@ TICKERS = [
 ]
 
 # Set the start and end dates for the data
-start_date = "2022-05-17"
+start_date = "2022-01-01"
 end_date = "2023-05-17"
 
-# Iterate over the tickers and retrieve the data
-for ticker_tuple in TICKERS:
+# CouchDB info
+db_url = 'http://admin:mysecretpassword@172.26.135.198:5984/'
+dbname = 'mastodon_test_4'
 
-    data = ticker_tuple[0].history(start=start_date, end=end_date, interval="1h")
+def pushToCouch(json_string):
+    """
+    Inserts a document into a CouchDB database.
 
-    # Create a list to hold the data for each day
-    price_history = []
+    Args:
+        data: A JSON string containing the data to be inserted into CouchDB.
+        
+    Returns:
+        None
+    """
+    
+    try:
+        # Connect to the CouchDB server using the given URL and credentials
+        server = couchdb.Server(db_url)
+    except:
+        print("Failed to connect to server")
 
-    # Iterate over each row in the data and extract the required information
-    for index, row in data.iterrows():
-        date = index.strftime("%Y-%m-%d")
-        closing_price = row["Close"]
-        price_history.append({"date": date, "closingPrice": closing_price})
+    try:
+        # Select the database to use
+        db = server[dbname]
 
-    # Create a dictionary to store the data for each ticker
-    ticker_data = {
-        "Ticker Code":ticker_tuple[0].ticker,
-        "Description":ticker_tuple[1],
-        "Price History":price_history  
-    }
+        # Save the data to the database
+        db.save(json.loads(json_string))
 
-    # Convert the dictionary to JSON format
-    json_str = json.dumps(ticker_data, indent=4)
+    # Handle any other error
+    except Exception:
+        print("Failed to push to CouchDB")
+        traceback.print_exc()
 
-# # Save the JSON data to a file
-filename = "stock_data.json"
-with open(filename, "w") as file:
-    file.write(json_str)
+def main_function():
 
-print(f"Stock data saved to {filename}")
+    """
+    Inserts historical data within the specified time period for tickers given
+    into CouchDB.
+
+    """
+
+    # Iterate over the tickers and retrieve the data
+    for ticker_tuple in TICKERS:
+
+        data = ticker_tuple[0].history(
+            start=start_date, 
+            end=end_date, 
+            interval="1h"
+        )
+
+        # Create a list to hold the data for each day
+        price_history = []
+
+        # Iterate over each row in the data and extract the required information
+        for index, row in data.iterrows():
+            date = index.strftime("%Y-%m-%d")
+            closing_price = row["Close"]
+            price_history.append({"date": date, "closingPrice": closing_price})
+
+        # Create a dictionary to store the data for each ticker
+        ticker_data = {
+            "Ticker Code":ticker_tuple[0].ticker,
+            "Description":ticker_tuple[1],
+            "Price History":price_history  
+        }
+
+        # Convert the dictionary to JSON format
+        json_str = json.dumps(ticker_data, indent=4)
+
+        pushToCouch(json_str)
+        print("Stock data saved to Couch: ", ticker_data['Description'])
+    
+    print("Completed data push")
+
+main_function()
