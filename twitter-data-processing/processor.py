360498892d43099ea7fd49ae8ab48c9b1195f99e
patwdj <helenawidjojo@hotmail.com>
2023-05-23 22:55:36 +1000

    only save tweets with bbox

--- twitter-data-processing/processor.py
+++ twitter-data-processing/processor.py
@@ -159,13 +159,11 @@ for prefix, event, value in parser:
         context_annotation.append(str(value))
     elif prefix == 'rows.item.doc.data.geo.place_id' and value != None:
         tweet['geo_place_id'] = str(value)
-        valid = True
     elif prefix == 'rows.item.doc.includes.places.item.geo.bbox.item' and value != None:
         geo_bbox.append(str(value))
         valid = True
     elif prefix == 'rows.item.doc.includes.places.item.full_name' and value != None:
         tweet['geo_full_name'] = str(value)
-        valid = True
     elif prefix == 'rows.item.doc.data.public_metrics.retweet_count' and value != None:
         tweet['retweet_count'] = str(value)
     elif prefix == 'rows.item.doc.data.public_metrics.reply_count' and value != None:
