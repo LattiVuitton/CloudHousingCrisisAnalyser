7ee443996a30b661c188892745bfe6f699a23be6
patwdj <helenawidjojo@hotmail.com>
2023-05-23 18:19:53 +1000

    updated twitter processor

--- twitter-data-processing/processor.py
+++ twitter-data-processing/processor.py
@@ -56,12 +56,73 @@ def contain_offensive(tweet):
     tweet = tweet.lower()
     return any(text in tweet for text in offensive_words)
 
+
+#to add suburb to tweets
+class Suburb():
+    def __init__(self, bbox, suburb_code, polygon):
+        self.id = suburb_code
+        self.left = min(bbox[0],bbox[2])
+        self.right = max(bbox[0],bbox[2])
+        self.top = max(bbox[1],bbox[3])
+        self.bottom = min(bbox[1],bbox[3])
+        self.centre = [(bbox[0] + bbox[2]) / 2,(bbox[1] + bbox[3]) / 2]
+        self.polygon = polygon
+polygon_file_path =  os.path.join(script_dir, "Merged-Aus-Polygon_2.geojson")
+
+with open(polygon_file_path, 'r') as f:
+    data = json.load(f)
+
+suburbs = []
+
+for obj in data['features']:
+    suburb = Suburb(
+        obj['properties']['bbox'],
+        obj['properties']['geography0'],
+        obj['geometry']['coordinates'][0]
+    )
+    suburbs.append(suburb)
+
+# Ray tracing
+def ray_tracing_method(x,y,poly):
+
+    n = len(poly)
+    inside = False
+
+    p1x,p1y = poly[0]
+    for i in range(n+1):
+        p2x,p2y = poly[i % n]
+        if y > min(p1y,p2y):
+            if y <= max(p1y,p2y):
+                if x <= max(p1x,p2x):
+                    if p1y != p2y:
+                        xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+                    if p1x == p2x or x <= xints:
+                        inside = not inside
+        p1x,p1y = p2x,p2y
+
+    return inside
+
+#start processing twitter data
 for prefix, event, value in parser:
 
     if prefix == 'rows.item.id':
-        if valid == True: #if previous tweet was valid i.e. have geo id
+
+        #if tweet is valid i.e. have geo id, we add it to be bulk sent
+        if valid == True: 
             tweet['context_annotation'] = context_annotation
             tweet['geo_bbox'] = geo_bbox
+            x_coord = (float(tweet['geo_bbox'][0]) + float(tweet['geo_bbox'][2]))/2
+            y_coord =(float(tweet['geo_bbox'][1]) + float(tweet['geo_bbox'][3]))/2
+
+            for sub in suburbs:
+                # In bounding box
+                if x_coord >= sub.left and x_coord <= sub.right and y_coord <= sub.top and y_coord >= sub.bottom:
+                    # Check if in polygon
+                    if ray_tracing_method(x_coord,y_coord,sub.polygon):
+                        tweet['geo_postcode'] = sub.id        
+
+            tweet['geo_point'] = [x_coord, y_coord]
+
             if tweet['lang'] == 'en':
                 tweet['offensive'] = contain_offensive(tweet['text'])
             else:
@@ -84,7 +145,7 @@ for prefix, event, value in parser:
         geo_bbox = []
 
         tweet_count += 1
-        tweet['id'] = str(value)
+        tweet['_id'] = str(value)
         
     elif prefix == 'rows.item.value.tokens' and value != None:
         tweet['tokens'] = str(value)
